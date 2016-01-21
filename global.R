ed5873791e09b55a6542d29152f5524b67bf5771
jsramos <ramos.cardona@gmail.com>
2016-01-21 00:31:36 -0600

    Republished with new members.

--- global.R
+++ global.R
@@ -3,7 +3,7 @@ rm(list = ls())
 
 # Meetup.com API key: 253275a5c18147e74c5b1e2a6708
 meetupurl <-
-  "https://api.meetup.com/2/profiles?&sign=true&photo-host=public&group_urlname=thedatapub&page=100&key=253275a5c18147e74c5b1e2a6708"
+  "https://api.meetup.com/2/profiles?&sign=true&photo-host=public&group_urlname=thedatapub&key=253275a5c18147e74c5b1e2a6708"
 
 # Libraries
 library(RCurl)
@@ -39,42 +39,51 @@ attendees <- data.frame(
     attendeesraw$answers,
     FUN = function(x) {
       elem <- x$answer[1]
-      as.numeric(ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3))))
+      ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3)))
     }
   )),
   cs = unlist(sapply(
     attendeesraw$answers,
     FUN = function(x) {
       elem <- x$answer[2]
-      as.numeric(ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3))))
+      ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3)))
     }
   )),
   swdev = unlist(sapply(
     attendeesraw$answers,
     FUN = function(x) {
       elem <- x$answer[3]
-      as.numeric(ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3))))
+      ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3)))
     }
   )),
   dataviz = unlist(sapply(
     attendeesraw$answers,
     FUN = function(x) {
       elem <- x$answer[4]
-      as.numeric(ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3))))
+      ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3)))
     }
   )),
   biz = unlist(sapply(
     attendeesraw$answers,
     FUN = function(x) {
       elem <- x$answer[5]
-      as.numeric(ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3))))
+      ifelse(is.null(elem), NA, gsub(',','', substr(elem, 1, 3)))
     }
   )),
   stringsAsFactors = F # Critical if yo don't want factors to screw up your transformation
 )
 
+answerlist <- sapply(select(attendees, -name), as.numeric, simplify = F)
+answerlist <- lapply(answerlist, function(x) {ifelse(x > 10, 10, x)})
+attendeesclean <- attendees %>% mutate(stats = answerlist[[1]],
+                                       cs = answerlist[[2]],
+                                       swdev = answerlist[[3]],
+                                       dataviz = answerlist[[4]],
+                                       biz = answerlist[[5]])
+
+
 # impute NA values with mean
-attendees <- attendees %>% mutate(
+attendeesclean <- attendeesclean %>% mutate(
   stats = ifelse(is.na(stats), mean(stats, na.rm = T), stats),
   cs = ifelse(is.na(cs), mean(cs, na.rm = T), cs),
   swdev = ifelse(is.na(swdev), mean(swdev, na.rm = T), swdev),
@@ -83,14 +92,14 @@ attendees <- attendees %>% mutate(
 )
 
 # Calculate avg for each attendee
-attendees <- mutate(attendees, avgscore = rowMeans(attendees[-1], na.rm = T))
+attendeesclean <- mutate(attendeesclean, avgscore = rowMeans(attendeesclean[-1], na.rm = T))
 
 # Create another dataframe in long format.
 attendeeslong <-
-  gather(attendees, key = profileattribute, value = score, c(-name))
+  gather(attendeesclean, key = profileattribute, value = score, c(-name))
 
 # Extract 1st name only for gender estimation
-attendees <- mutate(attendees, firstname = sapply(strsplit(attendees$name, " "), `[`, 1))
+attendeesclean <- mutate(attendeesclean, firstname = sapply(strsplit(attendeesclean$name, " "), `[`, 1))
 
 # Estimate gender using gender package, and using genderize.io API.
 # Since this is a costly operation, we save it to a file and if not present,
@@ -98,24 +107,24 @@ attendees <- mutate(attendees, firstname = sapply(strsplit(attendees$name, " "),
 if (file.exists('./namegenders.RDS')) {
     gendr <- readRDS('./namegenders.RDS')
 } else {
-  gendr <- gender(attendees$firstname, method = 'genderize')  
+  gendr <- gender(attendeesclean$firstname, method = 'genderize')  
   saveRDS(gendr, './namegenders.RDS')
 }
-attendees$gendr = gendr$gender
-attendees <- attendees %>% mutate(gendr = ifelse(is.na(gendr), 'NA', gendr))
+attendeesclean$gendr = gendr$gender
+attendeesclean <- attendeesclean %>% mutate(gendr = ifelse(is.na(gendr), 'NA', gendr))
 
 # Gender count
 gen <- data.frame(genero = c('mujeres', 'hombres', 'Sin respuesta'), 
-                  count = c(nrow(filter(attendees, gendr == 'female')),
-                            nrow(filter(attendees, gendr == 'male')),
-                            nrow(filter(attendees, gendr == 'NA'))
+                  count = c(nrow(filter(attendeesclean, gendr == 'female')),
+                            nrow(filter(attendeesclean, gendr == 'male')),
+                            nrow(filter(attendeesclean, gendr == 'NA'))
                   )
 )
 
 # Create 3rd dataframe with mean by discipline
 meandiscipline <- data.frame(discipline = c('stats','cs','swdev','dataviz', 'biz'), 
-                             meanscore = c(mean(attendees$stats),
-                                          mean(attendees$cs),
-                                          mean(attendees$swdev),
-                                          mean(attendees$dataviz),
-                                          mean(attendees$biz)))
+                             meanscore = c(mean(attendeesclean$stats),
+                                          mean(attendeesclean$cs),
+                                          mean(attendeesclean$swdev),
+                                          mean(attendeesclean$dataviz),
+                                          mean(attendeesclean$biz)))
